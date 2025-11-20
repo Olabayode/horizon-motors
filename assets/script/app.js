918e690b60ad696d2af656e8374b929e998ccc7e
Emmanuel Olure <emmanuelolabayode@gmail.com>
2025-11-19 23:12:57 -0600

    login modal

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open');
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+openModal.addEventListener('click', () => {
+  modal.showModal();
+});
+
 window.addEventListener('scroll', function() {
   const header = document.querySelector('header');
-  if (window.scrollY > 30) { 
+  if (window.scrollY > 20) { 
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 30) {  
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');  
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
+  });
 });
